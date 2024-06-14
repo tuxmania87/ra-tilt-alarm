3cf04ebf539a23ec2de9d3dc55d621e68c0357ac
tuxmania <robert@keinerspieltmitmir.de>
2024-06-14 17:48:41 +0200

    more logs

--- app.py
+++ app.py
@@ -19,16 +19,21 @@ def check_last_games(username, mode, interval, num_games):
     # check mode
     for g in games:
         if not g["rated"]:
+            logging.info(f"Failed because game {g['id']} is not rated")
             return False
 
         if g["perf"] != mode:
+            logging.info(f"Failed because game {g['id']} is not requested mode {mode}")
             return False
 
     # check times
-    current_start = time.time() * 1000
+    current_start = int(time.time()) * 1000
     for g in games:
 
         if current_start - g["lastMoveAt"] > interval:
+            logging.info(
+                f"Failed because of interval problem {current_start} {g['lastMoveAt']} game {g['id']} current {int(time.time()*1000)}"
+            )
             return False
 
         current_start = g["createdAt"]
@@ -38,9 +43,15 @@ def check_last_games(username, mode, interval, num_games):
         is_white = True if g["players"]["white"]["id"] == username.lower() else False
 
         if is_white and g["winner"] != "black":
+            logging.info(
+                f"Failed because game {g['id']} player is white but winner was not black"
+            )
             return False
 
         if not is_white and g["winner"] != "white":
+            logging.info(
+                f"Failed because game {g['id']} player is black but winner was not white"
+            )
             return False
 
     return True
